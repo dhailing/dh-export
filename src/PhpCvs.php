917aaa7c207770d823530eb1e068a353e31714b0
denghao <denghao@highnes.com>
2020-04-08 18:52:07 +0800

    修改csv导出

--- src/PhpCvs.php
+++ src/PhpCvs.php
@@ -20,28 +20,33 @@ class PhpCvs implements ExportInterface
     public function doExport()
     {
         // TODO: Implement doExport() method.
-        $string = '';
-        $colum = count($this->title);
-        for ($i = 0; $i < $colum; $i++) {
-            $string .= $this->title[$i];
-        }
-        $string .= "\n";
-        $string = iconv('utf-8','GB18030', $string);
-        $listcount = count($this->data);
-        for ($j = 0; $j < $listcount; $j++) {
-            $row = array_keys($this->data[$j]);
-            for ($k = 0; $k < $colum; $k++) {
-                $string .= $row[$k];
+        set_time_limit(0);
+        $csvFileName = $this->fileName;
+        //设置好告诉浏览器要下载excel文件的headers
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment; filename="' . $csvFileName . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        $fp = fopen('php://output', 'a');//打开output流
+        mb_convert_variables('GBK', 'UTF-8', $this->title);
+        fputcsv($fp, $this->title);//将数据格式化为CSV格式并写入到output流中
+        $accessNum = count($this->data);//从数据库获取总量，假设是一百万
+        for ($i = 1; $i <= $accessNum; $i++) {
+            //需要导出的数据
+            $rowData = [];
+            $rowNum = count($this->data[$i]);
+            for ($j = 0;$j < $rowNum; $j++) {
+                array_push($rowData, $this->data[$i][$j]);
             }
-            $string .= "\n";
+            mb_convert_variables('GBK', 'UTF-8', $rowData);
+            fputcsv($fp, $rowData);
+            //刷新输出缓冲到浏览器
+            ob_flush();
+            flush();//必须同时使用 ob_flush() 和flush() 函数来刷新输出缓冲。
         }
-        header("Content-type:text/csv");
-        header("Content-Disposition:attachment;filename=".$this->fileName . ".cvs");
-        header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
-        header('Expires:0');
-        header('Pragma:public');
-
-        echo $string;
-        exit(0);
+        fclose($fp);
+        exit();
     }
 }
